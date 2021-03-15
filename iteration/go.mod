ab3ea9bad4bfea48dfa2e053d05d23d0f8aa4bf4
Mariana Godoy <marianagsilva@usp.br>
2021-03-15 16:29:57 -0300

    Iteration section

---
+++ iteration/go.mod
@@ -0,0 +1,3 @@
+module github.com/marianagsilva/learn-go-with-tests/iteration
+
+go 1.16
---
+++ iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+func ExampleRepeat() {
+	repeating := Repeat("b")
+
+	fmt.Println(repeating)
+
+}
